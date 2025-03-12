e8bccb63e3680f4197f2cfc7aa1e57d881af9ace
Joaquim Escrihuela <xescrihuelc@outlook.com>
2025-03-12 13:23:18 +0100

    Corregir 'updatedArray' de const 'eraseJoke'

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
+let numJoke = 0;
+
+const emptyJokeList = () => {
+    const jokeList = document.getElementById("jokeList");
+    jokeList.innerHTML = "";
+}; // Hacer más tarde
+
 const obtainJoke = () => {
     fetch("https://api.chucknorris.io/jokes/random")
         .then((response) => response.json())
         .then((data) => {
             renderJoke(data.value);
+            saveJoke(data.value);
         })
         .catch((error) => {
             console.error("ERROR:", error);
@@ -15,13 +23,67 @@ const renderJoke = (joke) => {
     jokeDiv.classList.add("jokeBox");
     jokeDiv.innerHTML = `
     <p class="joke">${joke}</p>
-    <button type="button" class="bttnDeleteJoke">Eliminar</button>
+    <button type="button" class="bttnDeleteJoke" onclick="eraseJoke(${numJoke})">Eliminar</button>
     `;
     jokesList.appendChild(jokeDiv);
+    numJoke += 1;
 };
 
-// const saveJoke = (phrase) => {};
-// const loadJoke = (phrase) => {}
+const saveJoke = (phrase) => {
+    if (!localStorage.getItem("jokes")) {
+        localStorage.jokes = "[]";
+    }
+    const jokesArray = JSON.parse(localStorage.getItem("jokes"));
+    jokesArray.push(phrase);
+    const newData = JSON.stringify(jokesArray);
+    localStorage.setItem("jokes", newData);
+};
+
+const updateJoke = () => {
+    numJoke = 0;
+
+    //
+    const jokes = document.querySelectorAll(".bttnDeleteJoke");
+    //
+
+    jokes.forEach((joke) => {
+        joke.setAttribute("onclick", `eraseJoke(${numJoke})`);
+        console.log(localStorage.getItem("jokes"));
+
+        numJoke += 1;
+    });
+
+    //
+};
+
+const eraseJoke = (number) => {
+    //
+    const divJokes = document.querySelectorAll(".jokeBox");
+    //
+    const jokeElement = divJokes[number];
+    jokeElement.remove();
+    //
+    const jokesArray = JSON.parse(localStorage.getItem("jokes"));
+    const updatedArray = jokesArray.splice(1, number);
+
+    //
+    const newData = JSON.stringify(updatedArray);
+    localStorage.setItem("jokes", newData);
+
+    //
+    updateJoke();
+};
+
+const loadJoke = () => {
+    if (!localStorage.getItem("jokes")) {
+        localStorage.jokes = "[]";
+    } else {
+        const jokesArray = JSON.parse(localStorage.getItem("jokes"));
+        jokesArray.forEach((joke) => {
+            renderJoke(joke);
+        });
+    }
+};
 
 // Variable Elements
 const getJoke = document.getElementById("fetchJoke");
@@ -29,3 +91,5 @@ const jokesList = document.getElementById("jokeList");
 
 // Triggers
 getJoke.addEventListener("click", obtainJoke);
+
+loadJoke();
